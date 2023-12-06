d65aa3f45b825ade25f6aab5d5184bd45c747edf
Marko Okreša <marko@Markos-MacBook-Pro.local>
2023-12-07 00:58:32 +0100

    add backend project files

---
+++ Backend/apank/src/main/java/hr/fer/projekt/apank/ApankApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.projekt.apank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApankApplication.class, args);
+	}
+
+}
---
+++ Backend/apank/src/test/java/hr/fer/projekt/apank/ApankApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.projekt.apank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
